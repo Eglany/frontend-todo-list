b57c3afc8676415afeb066dc98c154e2531af325
Eglany Junior <contas.eglanyjr@gmail.com>
2022-06-29 14:50:50 -0400

    :lipstick:  shaping initial sections

---
+++ src/pages/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Main() {
+  return (
+    <main>
+      <aside id="new-task">
+        <h1>criar nova tarefa</h1>
+      </aside>
+      <section id="tasks">
+        <h1 id="title-tasks">Lista de tarefas</h1>
+      </section>
+    </main>
+  );
+}
+
+export default Main;
